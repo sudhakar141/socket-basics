53642a7432e8d0de8818069a2290e00db192fa9a
Sudhakar Mishra <sudhakar.strongmoments@gmail.com>
2018-11-08 14:21:50 +0530

    Added custom message event

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,8 @@ var socket = io();
 socket.on('connect', function(){
     console.log('Connected to socket.io to server!');
 });
+
+socket.on('message', function(message){
+    console.log('New message:');
+    console.log(message.text);
+});
